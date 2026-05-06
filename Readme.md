0e0f9c36071e0691a365eaa5b4ba71da1b0d0a8a
Franco <francoignacioromano08@gmail.com>
2026-05-06 15:12:30 -0300

    en

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Datos Personales
+- Curso: 6to 3ra
+- Grupo de Taller: 6.8
+- Tematica: Funko pop
+
+# Lenguajes de Programacion
+- HTML:
+- CSS:
+- JAVASCRIPT:
+
+## Estructura
---
+++ Script/db.js
@@ -0,0 +1,82 @@
+export const DB=[
+    {
+        ID:1,
+        Nombre:'Funko de ',
+        Precio:1500,
+        Stock:100,
+        Descripcion:'',
+        Imagen:""
+    },
+    {
+        ID:2,
+        Nombre:'Funko de ',
+        Precio:10000,
+        Stock:1,
+        Descripcion:'',
+        Imagen:""
+    },    
+    {
+        ID:3,
+        Nombre:'Funko de ',
+        Precio:3000,
+        Stock:29,
+        Descripcion:'',
+        Imagen:""
+    },
+    {
+        ID:3,
+        Nombre:'Funko de ',
+        Precio:2000,
+        Stock:50,
+        Descripcion:'',
+        Imagen:""
+    },
+    {
+        ID:4,
+        Nombre:'Funko de ',
+        Precio:1800,
+        Stock:32,
+        Descripcion:'',
+        Imagen:""
+    },
+    {
+        ID:5,
+        Nombre:'Funko de ',
+        Precio:3800,
+        Stock:43,
+        Descripcion:'',
+        Imagen:""
+    },
+    {
+        ID:5,
+        Nombre:'Funko de ',
+        Precio:1576,
+        Stock:78,
+        Descripcion:'',
+        Imagen:""
+    },
+    {
+        ID:6,
+        Nombre:'Funko de ',
+        Precio:1254,
+        Stock:4,
+        Descripcion:'',
+        Imagen:""
+    },
+    {
+        ID:7,
+        Nombre:'Funko de ',
+        Precio:1400,
+        Stock:12,
+        Descripcion:'',
+        Imagen:""
+    }, 
+    {
+        ID:8,
+        Nombre:'Funko de ',
+        Precio:1500,
+        Stock:5,
+        Descripcion:'',
+        Imagen:""
+    },                         
+]
